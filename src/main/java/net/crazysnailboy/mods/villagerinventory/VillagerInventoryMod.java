a4bac16467dd19e7429309543c8932d5f54f1b68
Chris Porter <microsoft-live@crazysnailboy.net>
2017-01-24 01:24:46 +0000

    fixed update url

--- src/main/java/net/crazysnailboy/mods/villagerinventory/VillagerInventoryMod.java
+++ src/main/java/net/crazysnailboy/mods/villagerinventory/VillagerInventoryMod.java
@@ -31,7 +31,7 @@ public class VillagerInventoryMod
 	public static final String MODNAME = "Villager Inventory Viewer";
 	public static final String VERSION = "1.0";
 	public static final String GUIFACTORY = "net.crazysnailboy.mods.villagerinventory.client.config.ModGuiFactory";
-	public static final String UPDATEJSON = "https://raw.githubusercontent.com/crazysnailboy/VillagerInventory/1.10.2/update.json";
+	public static final String UPDATEJSON = "https://raw.githubusercontent.com/crazysnailboy/VillagerInventory/master/update.json";
 	
 	
 	// mod instance
