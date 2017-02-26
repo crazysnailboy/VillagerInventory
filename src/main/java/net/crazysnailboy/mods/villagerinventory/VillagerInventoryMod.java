4a87e1214a6b6174e58a2b8f08c6a69ff8ce87ca
Chris Porter <github@crazysnailboy.net>
2017-02-26 18:47:02 +0000

    added config option for empty hand (issue #1)

--- src/main/java/net/crazysnailboy/mods/villagerinventory/VillagerInventoryMod.java
+++ src/main/java/net/crazysnailboy/mods/villagerinventory/VillagerInventoryMod.java
@@ -25,16 +25,16 @@ import net.minecraftforge.fml.relauncher.Side;
 
 
 @Mod(modid = VillagerInventoryMod.MODID, name = VillagerInventoryMod.MODNAME, version = VillagerInventoryMod.VERSION, updateJSON = VillagerInventoryMod.UPDATEJSON, guiFactory = VillagerInventoryMod.GUIFACTORY)
-public class VillagerInventoryMod 
+public class VillagerInventoryMod
 {
 
 	public static final String MODID = "villagerinventory";
 	public static final String MODNAME = "Villager Inventory Viewer";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.1";
 	public static final String GUIFACTORY = "net.crazysnailboy.mods.villagerinventory.client.config.ModGuiFactory";
 	public static final String UPDATEJSON = "https://raw.githubusercontent.com/crazysnailboy/VillagerInventory/master/update.json";
-	
-	
+
+
 	// mod instance
 	@Instance(VillagerInventoryMod.MODID)
 	public static VillagerInventoryMod instance;
@@ -45,15 +45,15 @@ public class VillagerInventoryMod
 	// network
 	private static SimpleNetworkWrapper network = NetworkRegistry.INSTANCE.newSimpleChannel(VillagerInventoryMod.MODID);
 
-	
-	
+
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		// initialize the configuration
 		ModConfiguration.preInit();
 		if (event.getSide() == Side.CLIENT) ModConfiguration.clientPreInit();
-		
+
 		// register the network messages
 		network.registerMessage(VillagerCareerMessage.MessageHandler.class, VillagerCareerMessage.class, 0, Side.CLIENT);
 	}
@@ -63,59 +63,65 @@ public class VillagerInventoryMod
 	{
 		// register with the event bus
 		MinecraftForge.EVENT_BUS.register(this);
-		
+
 		// register the gui handler
 		NetworkRegistry.INSTANCE.registerGuiHandler(VillagerInventoryMod.instance, guiHandler);
 	}
-	
+
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
 	}
-	
+
 
 	@SubscribeEvent
 	public void onPlayerEntityInteract(PlayerInteractEvent.EntityInteract event)
 	{
+		// if the villager inventory gui is enabled...
 		if (ModConfiguration.enableInventoryGui)
 		{
+			// if the event is running on the server, if the entity being interacted with is a villager, and if the player is sneaking...
 			if ((!event.getWorld().isRemote) && (event.getTarget() instanceof EntityVillager) && (event.getEntityPlayer().isSneaking()))
 			{
+				// if opening the gui requires the player to not be holding anything, check to ensure that the player isn't holding anything
+				if (ModConfiguration.requireEmptyHand && !event.getEntityPlayer().inventory.getCurrentItem().isEmpty()) return;
+
 				// cast the entity to type EntityVillager
 				EntityVillager villager = (EntityVillager)event.getTarget();
-				
+
 				// get the entity id of the villager
 				int entityId = villager.getEntityId();
-	
+
 				// and read it's profession and career from it's nbt data
 				NBTTagCompound compound = new NBTTagCompound();
 				villager.writeEntityToNBT(compound);
 				int professionId = compound.getInteger("Profession"); // int professionId = villager.getProfession();
 				int careerId = compound.getInteger("Career"); // int careerId = ((Integer)(ObfuscationReflectionHelper.getPrivateValue(EntityVillager.class, villager, "careerId", "field_175563_bv"))).intValue();
-	
+
 				// send a message to the client to tell it what the villager's career is
 				network.sendTo(new VillagerCareerMessage(entityId, careerId), (EntityPlayerMP)event.getEntityPlayer());
-				
-				// open the inventory gui				
+
+				// open the inventory gui
 				FMLNetworkHandler.openGui(event.getEntityPlayer(), VillagerInventoryMod.instance, ModGuiHandler.GUI_VILLAGER_INVENTORY, event.getWorld(), entityId, professionId, careerId);
-	
-				// cancel the event which would show the trading gui				
+
+				// cancel the event which would show the trading gui
 				event.setCanceled(true);
 			}
 		}
 	}
-	
-	
+
+
 	@SubscribeEvent
 	public void onEntityDeath(LivingDeathEvent event)
 	{
+		// if villager death drops are enabled...
 		if (ModConfiguration.enableDeathDrops)
 		{
 			// if the newly dead entity is a villager, and that villager was killed by a player...
 			if ((event.getEntityLiving() instanceof EntityVillager) && (event.getSource().getSourceOfDamage() instanceof EntityPlayerMP))
 			{
 				// iterate through the itemstacks in the villager's inventory
-				InventoryBasic inventory = ((EntityVillager)event.getEntityLiving()).getVillagerInventory(); 
+				InventoryBasic inventory = ((EntityVillager)event.getEntityLiving()).getVillagerInventory();
 				for ( int i = 0 ; i < inventory.getSizeInventory() ; i++ )
 				{
 					// remove the stack from the inventory and spawn it in the world
@@ -128,6 +134,6 @@ public class VillagerInventoryMod
 			}
 		}
 	}
-	
-	
+
+
 }
--- src/main/java/net/crazysnailboy/mods/villagerinventory/common/config/ModConfiguration.java
+++ src/main/java/net/crazysnailboy/mods/villagerinventory/common/config/ModConfiguration.java
@@ -14,15 +14,16 @@ import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
-public class ModConfiguration 
+public class ModConfiguration
 {
 	private static Configuration config = null;
 	private static ConfigEventHandler configEventHandler = new ConfigEventHandler();
 
-	public static boolean enableInventoryGui;
-	public static boolean enableDeathDrops;
-	
-	
+	public static boolean enableInventoryGui = true;
+	public static boolean enableDeathDrops = true;
+	public static boolean requireEmptyHand = false;
+
+
 	public static void preInit()
 	{
 		File configFile = new File(Loader.instance().getConfigDir(), VillagerInventoryMod.MODID + ".cfg");
@@ -30,88 +31,96 @@ public class ModConfiguration
 		config.load();
 		syncFromFile();
 	}
-	
-	public static void clientPreInit() 
+
+	public static void clientPreInit()
 	{
 		MinecraftForge.EVENT_BUS.register(new ConfigEventHandler());
 	}
-	
-	
-	public static Configuration getConfig() 
+
+
+	public static Configuration getConfig()
 	{
 		return config;
 	}
-	
-	
-	public static void syncFromFile() 
+
+
+	public static void syncFromFile()
 	{
 		syncConfig(true, true);
 	}
 
-	public static void syncFromGUI() 
+	public static void syncFromGUI()
 	{
 		syncConfig(false, true);
 	}
 
-	public static void syncFromFields() 
+	public static void syncFromFields()
 	{
 		syncConfig(false, false);
 	}
-	
 
-	
-	private static void syncConfig(boolean loadConfigFromFile, boolean readFieldsFromConfig) 
+
+
+	private static void syncConfig(boolean loadConfigFromFile, boolean readFieldsFromConfig)
 	{
-		
-		if (loadConfigFromFile) 
+
+		if (loadConfigFromFile)
 		{
 			config.load();
 		}
-		
-		Property propEnableInventoryGUI = config.get(Configuration.CATEGORY_GENERAL, "enableInventoryGui", true, "");
+
+		Property propEnableInventoryGUI = config.get(Configuration.CATEGORY_GENERAL, "enableInventoryGui", enableInventoryGui, "");
 		propEnableInventoryGUI.setLanguageKey("options.enableInventoryGui");
 		propEnableInventoryGUI.setRequiresMcRestart(false);
-		
-		Property propEnableDeathDrops = config.get(Configuration.CATEGORY_GENERAL, "enableDeathDrops", true, "");
+
+		Property propRequireEmptyHand = config.get(Configuration.CATEGORY_GENERAL, "requireEmptyHand", requireEmptyHand, "");
+		propRequireEmptyHand.setLanguageKey("options.requireEmptyHand");
+		propRequireEmptyHand.setRequiresMcRestart(false);
+
+		Property propEnableDeathDrops = config.get(Configuration.CATEGORY_GENERAL, "enableDeathDrops", enableDeathDrops, "");
 		propEnableDeathDrops.setLanguageKey("options.enableDeathDrops");
 		propEnableDeathDrops.setRequiresMcRestart(false);
 
-		
+
 		try
 		{
 			propEnableInventoryGUI.setConfigEntryClass(ModGuiConfigEntries.BooleanEntry.class);
+			propRequireEmptyHand.setConfigEntryClass(ModGuiConfigEntries.BooleanEntry.class);
 			propEnableDeathDrops.setConfigEntryClass(ModGuiConfigEntries.BooleanEntry.class);
-			
+
 			List<String> propOrderGeneral = new ArrayList<String>();
 			propOrderGeneral.add(propEnableInventoryGUI.getName());
+			propOrderGeneral.add(propRequireEmptyHand.getName());
 			propOrderGeneral.add(propEnableDeathDrops.getName());
 			config.setCategoryPropertyOrder(Configuration.CATEGORY_GENERAL, propOrderGeneral);
-			
+
 		}
 		catch(NoClassDefFoundError e) { }
 
-		
-		if (readFieldsFromConfig) 
+
+		if (readFieldsFromConfig)
 		{
 			enableInventoryGui = propEnableInventoryGUI.getBoolean();
+			requireEmptyHand = propRequireEmptyHand.getBoolean();
 			enableDeathDrops = propEnableDeathDrops.getBoolean();
 		}
-		
+
 		propEnableInventoryGUI.set(enableInventoryGui);
+		propRequireEmptyHand.set(requireEmptyHand);
 		propEnableDeathDrops.set(enableDeathDrops);
-		
-		if (config.hasChanged()) 
+
+		if (config.hasChanged())
 		{
 			config.save();
 		}
-		
+
 	}
-	
-	
-	public static class ConfigEventHandler 
+
+
+	public static class ConfigEventHandler
 	{
 		@SubscribeEvent
-		public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) 
+		public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event)
 		{
 			if (VillagerInventoryMod.MODID.equals(event.getModID()) && !event.isWorldRunning())
 			{
@@ -119,5 +128,5 @@ public class ModConfiguration
 			}
 		}
 	}
-	
+
 }
